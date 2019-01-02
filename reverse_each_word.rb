b606e77a7be9e1336a0572906a444612944b574b
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 04:13:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,5 +9,7 @@ def reverse_each_word(string)
   collect_array = string.split(" ")
   new_collect_array = []
   collect_array.collect do |string|
-    new_collect_array << string.
+    new_collect_array << string.reverse!
+  end 
+  new_collect_array
 end
